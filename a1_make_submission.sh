535ad5054bd7fd995967a15b7de949a9311d160a
rohankalbag <kalbag.rohan.r@gmail.com>
2023-11-17 00:21:40 +0530

    made minor changes

--- a1_make_submission.sh
+++ a1_make_submission.sh
@@ -1,3 +1,3 @@
 cp -r Assignment1 20d170033_Assignment1
-tar --exclude='*.ghw' -cvzf 20d170033_Assignment1.tgz 20d170033_Assignment1
+tar --exclude='*.ghw' --exclude='*.tgz' -cvzf 20d170033_Assignment1.tgz 20d170033_Assignment1
 rm -rf 20d170033_Assignment1
--- a2_make_submission.sh
+++ a2_make_submission.sh
@@ -1,3 +1,3 @@
 cp -r Assignment2 20d170033_Assignment2
-tar --exclude='*.ghw' -cvzf 20d170033_Assignment2.tgz 20d170033_Assignment2 
+tar --exclude='*.ghw' --exclude='*.tgz' -cvzf 20d170033_Assignment2.tgz 20d170033_Assignment2 
 rm -rf 20d170033_Assignment2
