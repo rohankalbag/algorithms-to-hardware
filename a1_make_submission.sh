c3fe825d21116b102b70920ac548424cfd3919bb
rohankalbag <kalbag.rohan.r@gmail.com>
2023-11-17 00:11:32 +0530

    added assignment files and removed .ghw overheads

--- a1_make_submission.sh
+++ a1_make_submission.sh
@@ -1,3 +1,3 @@
 cp -r Assignment1 20d170033_Assignment1
-tar -cvzf 20d170033_Assignment1.tgz 20d170033_Assignment1
+tar --exclude='*.ghw' -cvzf 20d170033_Assignment1.tgz 20d170033_Assignment1
 rm -rf 20d170033_Assignment1
